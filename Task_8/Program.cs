6211c12e3fef48fd053c1ee81549c3c2173227a2
Евгений <redcheeken@yandex.ru>
2022-08-25 19:06:48 +0300

    Task_8

---
+++ Task_8/Program.cs
@@ -0,0 +1,11 @@
+﻿int x;
+Console.WriteLine("Введите число ");
+x = Convert.ToInt32(Console.ReadLine());
+int y = 1;
+while(y <= x)
+{
+    if (y % 2 == 0){
+        Console.WriteLine(y);
+    }
+    y+=1;
+}
