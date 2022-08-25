1ebf15b910104b3a0d8e9bc5f01a6874b0bdd13e
Евгений <redcheeken@yandex.ru>
2022-08-25 18:42:12 +0300

    Task_24

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int x;
+Console.WriteLine("Введите первое число ");
+x = Convert.ToInt32(Console.ReadLine());
+int y;
+Console.WriteLine("Введите второе число ");
+y = Convert.ToInt32(Console.ReadLine());
+
+if ( x > y)
+{
+Console.Write ("Большее число ");
+Console.WriteLine (x);
+}
+else
+{
+Console.Write ("Большее число ");
+Console.WriteLine (y);
+}
