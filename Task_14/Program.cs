5b4606413a518b6093283204eb9e6b7548597ac4
Евгений <redcheeken@yandex.ru>
2022-08-27 18:24:33 +0300

    Task_14

---
+++ Task_14/Program.cs
@@ -0,0 +1,9 @@
+﻿int x;
+Console.WriteLine("Введите число: ");
+x = Convert.ToInt32(Console.ReadLine());
+
+if (x % 7 == 0)
+if (x % 23 == 0)
+Console.WriteLine("Число кратное 7 и 23");
+else
+Console.WriteLine("Не делится на 7 и 23 без остатка");
---
+++ Task_9/Program.cs
@@ -0,0 +1,7 @@
+﻿Random rand = new Random();
+int num = rand.Next (100, 1000);
+Console.WriteLine(num);
+int a1 = num / 100;
+int a2 = num % 10;
+Console.Write(a1);
+Console.WriteLine(a2);
